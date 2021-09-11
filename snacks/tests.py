29cd4f0a5917bab6e9c43afe909d99522a95bd2b
Leen Hazaimeh <leenhazaimeh94@gmail.coom>
2021-09-11 23:40:55 +0300

    done

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from .models import Snack
+
+# Create your tests here.
+
+class SnackTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='admin',
+            email='snack@gmail.com',
+            password='1234'
+        )
+        self.snack = Snack.objects.create(
+            title='potato',
+            purshaser=self.user,
+            description='potato and ketchup'
+        )
+
+    
+
+    def test_num_one(self):
+        self.assertEqual(str(self.snack), "potato")
+
+    
+
+    def test_num_two(self):
+        self.assertEqual(self.snack.title, 'potato')
+        self.assertEqual(str(self.snack.purshaser), 'snack@gmail.com')
+        self.assertEqual(self.snack.description, 'potato and ketchup')
+
+   
+    def test_snack_num_three(self):
+        expected = 200
+        response = self.client.get(reverse("snack_list"))
+        self.assertEqual(response.status_code, expected)
+        self.assertContains(response, "potato")
+        self.assertTemplateUsed(response, "snacks/snack_list.html")
+
+    
+
--- snacks/views.py
+++ snacks/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 from django.views.generic import ListView, DeleteView, DetailView, CreateView, UpdateView
 from django.urls import reverse_lazy
 from .models import Snack
@@ -8,28 +6,29 @@ from .models import Snack
 
 
 class SnackListView(ListView):
-    template_name = 'snacks/snack_list.html'
+    template_name = 'snack_list.html'
     model = Snack
 
 
 class SnackDetailsView(DetailView):
-    template_name = 'snacks/snack_detail.html'
+    template_name = 'snack_detail.html'
     model = Snack
+    
 
 
 class SnackCreateView(CreateView):
-    template_name = 'snacks/snack_create.html'
+    template_name = 'snack_delete.html'
     model = Snack
     fields = ['title', 'purshaser', 'description']
 
 
 class SnackUpdateView(UpdateView):
-    template_name = 'snacks/snack_update.html'
+    template_name = 'snack_update.html'
     model = Snack
     fields = ['title', 'purshaser', 'description']
 
 
 class SnackDeleteView(DeleteView):
-    template_name = 'snacks/snack_delete.html'
+    template_name = 'snack_delete.html'
     model = Snack
     success_url = reverse_lazy('snack_list')
